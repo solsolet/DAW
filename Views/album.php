c576b0b98ffb60aad756639b63930762cc76c749
sebascadpi <sebascadpi@gmail.com>
2022-11-23 20:10:25 +0100

    cosas hechas

--- Views/album.php
+++ Views/album.php
@@ -6,46 +6,60 @@
     include "inc/cabecera.php";
     include "inc/conect.php"; 
     if(isset($nombre)){
-    echo "<section>";
-        echo "<h2>Ver álbum</h2>";
-        
-        $sentencia = 'SELECT a.descripcion, COUNT(*) as total
-            FROM `albumes` a, `fotos` f, `usuarios` u
-            WHERE a.titulo = "'.$nombre[1].'" AND "'.$nombre[0].'" = u.nomUsuario AND u.idUsuario = a.usuario AND a.idAlbum = f.album ';
-        include "inc/request.php";
-        //muestra datos
-        $fila = $resultado->fetch_assoc();
-        
+        echo "<section>";
+            echo "<h2>Ver álbum</h2>";
+            //la f.fecha es la de la foto o la de registro??
+            $sentencia = 'SELECT a.descripcion, COUNT(*) as total, MIN(f.fecha) as ultima, MAX(f.fecha) as primera
+                FROM `albumes` a, `fotos` f, `usuarios` u
+                WHERE a.titulo = "'.$nombre[1].'" AND "'.$nombre[0].'" = u.nomUsuario AND u.idUsuario = a.usuario AND a.idAlbum = f.album ';
+            include "inc/request.php";
+            //muestra datos
+            $fila = $resultado->fetch_assoc();
+            
             echo<<<hereDOC
                 <h3>{$fila['total']} fotos</h3>
                 <h4>{$fila['descripcion']}</h4>
+                <p>De {$fila['ultima']} al {$fila['primera']}</p>
             hereDOC;    
 
-        //para sacar paises
-        $sentencia = 'SELECT *
+            //para sacar paises
+            $sentencia = 'SELECT DISTINCT *
             FROM `albumes` a, `fotos` f, `usuarios` u, `paises` p
             WHERE a.titulo = "'.$nombre[1].'" AND "'.$nombre[0].'" = u.nomUsuario AND u.idUsuario = a.usuario AND a.idAlbum = f.album 
-                AND pais = idPais';
-        include "inc/request.php";
-
-        echo "<div class='image-grid'>";
-        //muestra fotos
-        $sentencia = 'SELECT *
-        FROM `albumes` a, `fotos` f, `usuarios` u
-        WHERE a.titulo = "'.$nombre[1].'" AND "'.$nombre[0].'" = u.nomUsuario AND u.idUsuario = a.usuario AND a.idAlbum = f.album ';
-        include "inc/request.php";
-        while($fila = $resultado->fetch_assoc() ) {
-            echo<<<hereDOC
-                <article class="carta"><a href="foto/{$fila['idFotos']}"><img src={$fila['fichero']} alt={$fila['alternativo']}></a>
-                    <h3>{$fila['titulo']}</h3>
-                    <p>{$fila['descripcion']}</p>
-                </article>
-            hereDOC;
+                AND f.pais = idPais';
+            include "inc/request.php";
+            echo "<p>";
+            while($fila = $resultado->fetch_assoc() ) {
+                echo "
+    {$fila['nomPais']} ";
+            }
+            echo "</p>"; //posar + bonico
+            //muestra fotos
+            echo "<div class='image-grid'>";
             
+            $sentencia = 'SELECT *
+            FROM `albumes` a, `fotos` f, `usuarios` u
+            WHERE a.titulo = "'.$nombre[1].'" AND "'.$nombre[0].'" = u.nomUsuario AND u.idUsuario = a.usuario AND a.idAlbum = f.album ';
+            include "inc/request.php";
+            while($fila = $resultado->fetch_assoc() ) {
+                echo<<<hereDOC
+                    <article class="carta"><a href="foto/{$fila['idFotos']}"><img src={$fila['fichero']} alt={$fila['alternativo']}></a>
+                        <h3>{$fila['titulo']}</h3>
+                        <p>{$fila['descripcion']}</p>
+                    </article>
+                hereDOC;
+                
+            }
+
+        echo "</div></section>";
+
+        //si l'usuari que entra al album es el propietari
+        if($nombre[0] == $_SESSION['usuario']){
+            debug("printeame un botón to guapo");
+            echo '<a href="afalbum" class="btn">Añadir foto a álbum</a>';
         }
 
-    echo "</div></section>";  
-    include "inc/close.php"; 
-    include "inc/footer.php";
+        include "inc/close.php"; 
+        include "inc/footer.php";
     }
 ?>
